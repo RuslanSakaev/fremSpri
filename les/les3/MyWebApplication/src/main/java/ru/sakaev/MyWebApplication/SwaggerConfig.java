8022241003bacfe3132a885f9295079d1deba1c3
Sakaev Ruslan <hardist@mail.ru>
2024-01-13 16:25:17 +0300

    Добавил код к 3 лекции и с 2 семинара

---
+++ les/les3/MyWebApplication/src/main/java/ru/sakaev/MyWebApplication/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package ru.sakaev.MyWebApplication;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springshop-public")
+                .packagesToScan("ru.sakaev.MyWebApplication")
+                .build();
+    }
+}
+
