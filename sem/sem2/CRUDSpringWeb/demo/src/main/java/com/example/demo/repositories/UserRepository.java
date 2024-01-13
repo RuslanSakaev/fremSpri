b4a2e1d67e25c9f9a15aa1d838d532256a8f31e6
Sakaev Ruslan <hardist@mail.ru>
2024-01-13 17:09:30 +0300

    ДЗ к семирнару 2. Исправил ошибку при сохранении записи, добавил функцию удаления

--- sem/sem2/CRUDSpringWeb/demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++ sem/sem2/CRUDSpringWeb/demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -31,11 +31,14 @@ public class UserRepository {
     }
 
     public User save(User user) {
-        String sql = "INSERT INTO userTable VALUES (NULL, ?, ?)";
+        String sql = "INSERT INTO userTable (firstName, lastName) VALUES (?, ?)";
         jdbc.update(sql, user.getFirstName(), user.getLastName());
-        return  user;
+        return user;
     }
 
-    //public void deleteById(int id)
-    //"DELETE FROM userTable WHERE id=?"
+    public void deleteById(int id) {
+        String sql = "DELETE FROM userTable WHERE id = ?";
+        jdbc.update(sql, id);
+    }
+
 }
--- sem/sem2/CRUDSpringWeb/demo/src/main/java/com/example/demo/service/UserService.java
+++ sem/sem2/CRUDSpringWeb/demo/src/main/java/com/example/demo/service/UserService.java
@@ -25,5 +25,8 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    //public void deleteById(int id)
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+
 }
