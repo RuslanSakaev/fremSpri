4bdca5dce6e2f4bd66d0630398d70920b08cac5f
Rus <hardist@mail.ru>
2024-02-08 21:03:18 +0300

    Код семинара и Решение задачи к 6 семинару

---
+++ sem/sem9/sem9/src/main/java/ru/sakaev/sem9/Sem9Application.java
@@ -0,0 +1,13 @@
+package ru.sakaev.sem9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem9Application.class, args);
+	}
+
+}
